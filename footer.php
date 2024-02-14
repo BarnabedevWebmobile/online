b40f37af362ffb761c504b2da05aa78efc4c0bb0
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2024-02-14 11:32:46 +0100

    ajout footer

--- footer.php
+++ footer.php
@@ -1,20 +1,50 @@
-<footer class="mt-5 col-12  d-flex flex-column flex-md-row justify-content-around ">
-        <span class="col-md-1 col-4 mt-4">
-            <ul>
-                <h3>A propos</h3>
-                <li>mention légales</li>
-                <li>nous contacter</li>
-            </ul>
-        </span>
-        <div class="d-flex col-md-6 col-4 flex-column-reverse flex-md-row justify-content-between ">
-            <h4 class="mt-4 text-center ">design by berta@bretzeldesign</h4>
-            <span class="mt-4 col-4 ms-md-5">
+    <footer class="footer">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <span class="col-md-3 col-12 text-center">
+                <h4>qui sommes-nous ?</h1>
                 <ul>
-                    <h3>Nos réseaux sociaux </h3>
-                    <li><a href="https://twitter.com/DRAFPICA_Occ" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
-                    <li><a href="https://fr.linkedin.com/school/greta-occitanie/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li>Présentation</li>
+                    <li>L'équipe</li>
+                    <li>CFA du <br>numérique</li>
+                    <li>Actualités</li>
                 </ul>
             </span>
+            <span class="col-md-3 col-12 text-center">
+                <h4>infos pratiques</h4>
+                <ul>
+                    <li>Nos centres de formation</li>
+                    <li>Calendrier des formations</li>
+                    <li>Financer votre formation</li>
+                    <li>FAQ : Foire Aux Questions</li>
+                </ul>
+            </span>
+            <span class="col-md-3 col-12 text-center">
+                <h4>les formations</h4>
+                <ul>
+                    <li>Catalogue des formations</li>
+                    <li>Les formations diplômantes</li>
+                    <li>POE :  Préparation à l'Emploi</li>
+                    <li>Agenda des formations</li>
+                </ul>
+            </span>
+            
+        </div>
+        <div class="container footr mt-3">
+            <span class="col-12 text-center">
+                <ul>
+                    <h3>© 2024 GRETA, Tous droits réservés | Mentions légales</h3>
+                </ul>
+            </span>
+        <h4 class="mt-4 text-center ">design by berta@bretzeldesign</h4>
+            <div class="container ">
+                <span class="text-center col-12">
+                    <ul>
+                        <h3>Nos réseaux sociaux : <br> <button onclick="" class="btn btn-primary">Contactez-nous.</button></h3>
+                        <li><a href="https://twitter.com/DRAFPICA_Occ" target="_blank"><i class="fa-brands fa-x-twitter x_twitter"></i></a></li>
+                        <li><a href="https://fr.linkedin.com/school/greta-occitanie/" target="_blank"><i class="linked_in fa-brands fa-linkedin"></i></a></li>
+                    </ul>
+                </span>
+            </div>
         </div>
     </footer>
 </body>
