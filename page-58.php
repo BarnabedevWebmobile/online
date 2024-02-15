a0dd1271e5b1fa86478336ae0eec155f5d084416
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-02-15 14:34:06 +0100

    14h32 15fév
    pages annexes steph

---
+++ page-58.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+
+
+<div class='w-25 vide'></div>
+
+<div class="text-center fw-bold fs-1">
+    <?php the_title(); ?>
+</div>
+
+
+
+<?php get_footer(); ?>
+<?php wp_link_pages(); ?>
