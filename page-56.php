90d7efb77fa568b4e44a9082fd54ef0f4425ee01
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-02-16 15:37:01 +0100

    15h36 16fév2024
    ajout pictures et peaufinage des pages annexes

--- page-56.php
+++ page-56.php
@@ -5,17 +5,17 @@
 
 <div class='w-25 vide'></div>
 <h1 class="text-center">Formulaire de contact</h1>
-<div class="row">
+<div>
     <div class="col-xl-4">
     </div>
-            <div class="col-12 col-xl-4 bg-secondary">
-                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                        <div class="form d-flex justify-content-center">
-                            <?php the_content(); ?>
-                        </div>
-                    <?php endwhile; ?>
-                    <?php endif; ?>
-            </div>
+    <div class="col-12 col-xl-4 bg-secondary">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="form d-flex justify-content-center">
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
+    </div>
     <div class="col-xl-4">
     </div>
 </div>
--- page-57.php
+++ page-57.php
@@ -9,8 +9,8 @@
 
 <div class='w-25 vide'></div>
 
-<div class="text-center fw-bold fs-1">
-    <?php the_title(); ?>
+<div class="d-flex flex-column">
+    <h1 class="text-center fw-bold fs-1"><?php the_title(); ?></h1>
 
     <div>
 
@@ -35,10 +35,10 @@
                   ?>
                   
                   <div class="d-flex flex-md-row border border-dark border-3 m-5 p-5">
-                    <img src="<?php echo $posted ;?>" class="img-fluid">
-                    <div class="d-flex flex-column">
-                      <p class=""><?php echo $title;?></p>
-                      <p class=""><?php echo $descr;?></p>
+                    <img src="<?php echo $posted ;?>" class="img-fluid w-25">
+                    <div class="d-flex flex-column w-100">
+                      <p class="text-center fs-2"><?php echo $title;?></p>
+                      <p class="text-center fs-3"><?php echo $descr;?></p>
                     </div>                 
                   </div>
 
@@ -49,7 +49,7 @@
                 // 4. On réinitialise à la requête principale (important)
                 wp_reset_postdata();
               ?>    
-            </div>
+    </div>
 </div>
 
 
